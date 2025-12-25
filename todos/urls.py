f45013414424bca91a47040f3d205470c8a0e8cf
miladzarein <134214582+miladzarein@users.noreply.github.com>
2025-12-25 20:52:36 +0330

    add route for TodoDetailAPIView by id

--- todos/urls.py
+++ todos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TodoListCreateAPIView
+from .views import TodoListCreateAPIView,TodoDetailAPIView
 
 
 
 
 urlpatterns = [
-    path('todos/',TodoListCreateAPIView.as_view())
+    path('todos/',TodoListCreateAPIView.as_view()),
+    path('todos/<int:pk>/',TodoDetailAPIView.as_view())
 ]
