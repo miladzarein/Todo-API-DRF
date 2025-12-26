56a994790bff1c2b7448730864b82e2c1e5e1b53
miladzarein <134214582+miladzarein@users.noreply.github.com>
2025-12-26 18:35:34 +0330

    Add docstring to TodoListCreateAPIView and clean up formatting

--- todos/views.py
+++ todos/views.py
@@ -3,23 +3,23 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Todo
 from .serializers import TodoSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 
 class TodoListCreateAPIView(APIView):
     """List all todos or create a new one."""
-
+    permission_classes = [IsAuthenticated]
 
     def get(self,request):
-        todos = Todo.objects.all()
+        todos = Todo.objects.filter(owner=request.user)
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data)
     
 
     def post(self, request):
-        serializer = TodoSerializer(data=serializer.data)
+        serializer = TodoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(owner=request.user)
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
