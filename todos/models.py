4d7afe0e176d5c6d9cb66914e5ccc9fce1858525
miladzarein <134214582+miladzarein@users.noreply.github.com>
2025-12-28 18:32:47 +0330

    add Tenant model for future multi-tenancy

--- todos/models.py
+++ todos/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+class Tenant(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE,related_name='owned_tenants')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    
 class Todo(models.Model):
     owner = models.ForeignKey(User,on_delete=models.CASCADE,related_name='todos')
     title = models.CharField(max_length=200)
