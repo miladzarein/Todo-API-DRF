57d2961dfe8e45fbd4d301392cc0883046dba7f5
miladzarein <134214582+miladzarein@users.noreply.github.com>
2025-12-28 19:04:59 +0330

    introduce Tenant serializer for multi-tenancy

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Todo
+from .models import Todo,Tenant,UserProfile
 
 
+
+class TenantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tenant
+        fields = ['id','name','owner','created_at']
+        read_only_fields = ['id','owner','created_at']
 
 
 class TodoSerializer(serializers.ModelSerializer):
