21d8dc8cd07a6664bf5d3184f0b641d8edf4f1f1
miladzarein <134214582+miladzarein@users.noreply.github.com>
2025-12-29 17:41:56 +0330

    Add UserProfileSerializer and update serializers for multi-tenant support

--- todos/serializers.py
+++ todos/serializers.py
@@ -11,6 +11,17 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password':{'write_only':True}}
 
 
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
+    
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'username', 'email', 'tenant', 'role']
+        read_only_fields = ['id', 'username', 'email', 'tenant']
+
+
 class TenantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tenant
