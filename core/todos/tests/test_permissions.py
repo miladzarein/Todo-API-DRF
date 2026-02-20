8a1bcfba484aec9e4067fae38708fa9559d9ce28
miladzarein <134214582+miladzarein@users.noreply.github.com>
2026-02-20 17:27:05 +0330

    Add test for IsTenantMember permission with authenticated user

---
+++ core/todos/tests/test_permissions.py
@@ -0,0 +1,19 @@
+import pytest
+from django.test import RequestFactory
+from django.contrib.auth.models import User, AnonymousUser
+from todos.permissions import IsTenantMember
+from todos.models import Todo
+
+
+@pytest.mark.django_db
+def test_is_tenant_member_authenticated():
+    """Authenticated users should have permission"""
+    factory = RequestFactory()
+    user = User.objects.create_user(username="user1", password="pass123")
+
+    permission = IsTenantMember()
+    request = factory.get("/")
+    request.user = user
+
+    assert request.user.is_authenticated is True
+    assert permission.has_permission(request, None) is True
